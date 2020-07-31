9ae188b0cc677aa640bdb9cd306ca82c2614cf18
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-31 01:17:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,23 @@
 # Add  code here!
 def prime(number)
-  index = 0 
-  
+  if number > 1 && number < 4
+    return true
+  elsif number < 2 == true then return false 
+  elsif number.even? == true then return false 
+
+  else
+    
+a = (2 .. (number - 1)).to_a 
+
+a.collect do |i|
+ if (number % i) == 0 then false
+
+ 
+    
+   end 
+  end
+
+  end
+   false
 end
+  
