a4b063da6c60b8dc3222ab9f465bb7ad0b6eab0b
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-31 01:19:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,14 @@ def prime?(number)
   if number > 1 && number < 4
     return true
   elsif number < 2 == true then return false 
-  elsif number.even? == true then return true 
+  elsif number.even? == true then return false 
 
   else
     
 a = (2 .. (number - 1)).to_a 
 
 a.collect do |i|
- if (number % i) == 0 then false
+ if (number % i) == 0 then true
 
  
     
